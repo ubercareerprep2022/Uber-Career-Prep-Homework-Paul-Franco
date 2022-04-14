7f598244925b4483fe734b5cc0dc6094dcd66934
Paul Franco <69439997+francosae@users.noreply.github.com>
2022-04-14 03:19:12 -0400

    Update Part5.py

--- Assignment-1/Part5.py
+++ Assignment-1/Part5.py
@@ -1,1 +1,11 @@
-
+'''
+    def reverseIteratively(self):
+        prevNode = None
+        currNode = self.head
+        while currNode:
+            nextNode = currNode.next
+            currNode.next = prevNode
+            prevNode = currNode
+            currNode = nextNode
+        return prevNode
+'''
